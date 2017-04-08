d579dc15283a0b7452d6709f7626578f0afdc84e
ooctavia <ooctavia@terpmail.umd.edu>
2017-04-08 04:12:34 -0400

    Added Login and register page.

--- Tutor/app/src/main/java/com/example/osbertoctavianus/tutor/LoginActivity.java
+++ Tutor/app/src/main/java/com/example/osbertoctavianus/tutor/LoginActivity.java
@@ -1,7 +1,14 @@
 package com.example.osbertoctavianus.tutor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +16,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+        final TextView registerLink = (TextView) findViewById(R.id.tvRegisterHere);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
---
+++ Tutor/app/src/main/java/com/example/osbertoctavianus/tutor/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.example.osbertoctavianus.tutor;
+
+import android.icu.lang.UCharacterEnums;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etRePassword = (EditText) findViewById(R.id.etRePassword);
+        final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+
+
+    }
+}
